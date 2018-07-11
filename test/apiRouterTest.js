cbde2f80bb32c3385ad71904d151fe14ffd15f6f
Eric Phelan <epcphelan@gmail.com>
2018-07-11 21:05:20 +0200

    apiRouter test coverage

--- test/apiRouterTest.js
+++ test/apiRouterTest.js
@@ -2,10 +2,6 @@ const { expect } = require('chai');
 const ApiRouter = require('../libs/apiRouter');
 const SessionManager = require('../libs/sessionManager');
 
-const req ={};
-const res = {};
-const next = function(){};
-
 const app = {
   middlewares :[],
   use: function(fn){
@@ -14,7 +10,7 @@ const app = {
   routes:{},
   post: function(url, fn){
     this.routes[url] = fn
-  },
+  }
 };
 
 const endpoint ='http://some.api/v2';
@@ -50,5 +46,116 @@ describe('API Router', function(){
     it('should have a an sms endpoint route', function(){
       expect(app.routes[sms]).to.be.a('Function')
     })
+  });
+
+  describe('.sendError()', function(){
+    const SERVER_ERROR = {
+      success: false,
+      error: {
+        code: 500,
+        type: "api_server",
+        message: "API server not responding.",
+        detail: "[CRITICAL] The API server is not responding."
+      }
+    };
+    const res = {
+      head:null,
+      body:null,
+      writeHead: function (val){
+        this.head = val;
+      },
+      end: function (body){
+        this.body = body;
+      }
+    };
+    ApiRouter.sendError(res);
+    it('should send  error with 200 header', function(){
+      expect(res.head).to.equal(200)
+    });
+    it('should send error object', function(){
+      expect(res.body).to.equal(JSON.stringify(SERVER_ERROR))
+    })
+  });
+
+  describe('.sendResponse()', function(){
+    const Response = {
+      head:null,
+      body:null,
+      cookies:{},
+      writeHead: function (val){
+        this.head = val;
+      },
+      end: function (body){
+        this.body = body;
+      },
+      clearCookie: function(){
+        this.cookies = {}
+      },
+      append:function(key,value){
+        this.cookies[key] = value
+      }
+    };
+    describe('no cookies', function(){
+      const apiResponse = {
+        hello:"world"
+      };
+      const res = Object.assign({},Response);
+      const bodyText = JSON.stringify(apiResponse);
+      ApiRouter.sendResponse(res,{text:bodyText});
+
+      it('should send  error with 200 header', function(){
+        expect(res.head).to.equal(200)
+      });
+
+      it('should send error object', function(){
+        expect(res.body).to.equal(bodyText)
+      })
+    });
+    describe('cookies', function(){
+      const loginResponse = {
+        requested:"user.login",
+        success:true,
+        data:{
+          sessionToken:'123456789'
+        }
+      };
+      const logoutResponse = {
+        requested:"user.logout",
+        success:true,
+        data:{}
+      };
+
+
+      describe('set cookie', function(){
+        const res = Object.assign({},Response);
+        const body = JSON.stringify(loginResponse );
+        ApiRouter.sendResponse(res,{text:body});
+        it('should send 200 header', function(){
+          expect(res.head).to.equal(200)
+        });
+
+        it('should set cookie header', function(){
+          expect(res.cookies['Set-Cookie']).to.equal('parsonySession=123456789; HttpOnly');
+        });
+      });
+
+      describe('clear cookie', function(){
+        const res = Object.assign({},Response);
+        const logout = JSON.stringify(logoutResponse);
+        const login = JSON.stringify(loginResponse );
+        ApiRouter.sendResponse(res,{text:login});
+        ApiRouter.sendResponse(res,{text:logout});
+
+        it('should send 200 header', function(){
+          expect(res.head).to.equal(200)
+        });
+
+        it('should clear cookie header', function(){
+          expect(res.cookies['Set-Cookie']).to.equal(undefined);
+        })
+      })
+
+    })
+
   })
 });
